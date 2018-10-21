7d1c7d8e7c8ac51d72df361f1e281e9d8b6983f1
AliaksandrLiashevich <43993109+AliaksandrLiashevich@users.noreply.github.com>
2018-10-21 17:05:04 +0300

    PUSH

---
+++ JaggedArraySorts/JaggedSorts.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortJaggedArrayLibrary
+{
+    delegate bool Comparator(SingleRow firstArg, SingleRow secondArg);
+    public static class JaggedSorts
+    {
+        static Comparator solver = null;
+        static SingleRow[] keysArray = null;
+        public static void SumOfRowSort(ref int[][] jaggedArray, bool mode)
+        {
+            keysArray = new SingleRow[jaggedArray.GetLength(0)];
+            SumRowValue(jaggedArray);
+            SetSortMode(mode);
+            BubleSort.Sort(ref keysArray, solver);
+            ArraySwapRows(ref jaggedArray);
+        }
+        public static void MaxRowValueSort(ref int[][] jaggedArray, bool mode)
+        {
+            keysArray = new SingleRow[jaggedArray.GetLength(0)];
+            MaxRowValue(jaggedArray);
+            SetSortMode(mode);
+            BubleSort.Sort(ref keysArray, solver);
+            ArraySwapRows(ref jaggedArray);
+        }
+        public static void MinRowValueSort(ref int[][] jaggedArray, bool mode)
+        {
+            keysArray = new SingleRow[jaggedArray.GetLength(0)];
+            MinRowValue(jaggedArray);
+            SetSortMode(mode);
+            BubleSort.Sort(ref keysArray, solver);
+            ArraySwapRows(ref jaggedArray);
+        }
+        private static void SumRowValue(int[][] jaggedArray)
+        {
+            for(int i = 0; i < jaggedArray.GetLength(0); i++)
+            {
+                keysArray[i].row = jaggedArray[i];
+                for (int j = 0; j < jaggedArray[i].Length; j++)
+                {
+                    keysArray[i].key += jaggedArray[i][j];
+                }
+            }
+        }
+        private static void MaxRowValue(int[][] jaggedArray)
+        {
+            int maxValue = 0;
+            for (int i = 0; i < jaggedArray.GetLength(0); i++)
+            {
+                maxValue = jaggedArray[i][0];
+                keysArray[i].row = jaggedArray[i];
+                for (int j = 0; j < jaggedArray[i].Length; j++)
+                {
+                    if (maxValue < jaggedArray[i][j])
+                    {
+                        maxValue = jaggedArray[i][j]; 
+                    }
+                }
+                keysArray[i].key = maxValue;
+            }
+        }
+        private static void MinRowValue(int[][] jaggedArray)
+        {
+            int minValue = 0;
+            for (int i = 0; i < jaggedArray.GetLength(0); i++)
+            {
+                minValue = jaggedArray[i][0];
+                keysArray[i].row = jaggedArray[i];
+                for (int j = 0; j < jaggedArray[i].Length; j++)
+                {
+                    if (minValue > jaggedArray[i][j])
+                    {
+                        minValue = jaggedArray[i][j];
+                    }
+                }
+                keysArray[i].key = minValue;
+            }
+        }
+        private static void ArraySwapRows(ref int[][] jaggedArray)
+        {
+            for (int i = 0; i < jaggedArray.GetLength(0); i++)
+            {
+                jaggedArray[i] = keysArray[i].row;
+            }
+        }
+        private static void SetSortMode(bool mode)
+        {
+            if (mode)
+                solver = AscendingSortRules;
+            else
+                solver = DescendingSortRules;
+        }
+        private static bool AscendingSortRules(SingleRow firstArg, SingleRow secondArg)
+        {
+            if (firstArg.key > secondArg.key)
+                return true;
+            else
+                return false;
+        }
+        private static bool DescendingSortRules(SingleRow firstArg, SingleRow secondArg)
+        {
+            if (firstArg.key > secondArg.key)
+                return false;
+            else
+                return true;
+        }     
+    }
+    internal static class BubleSort
+    {
+        public static void Sort(ref SingleRow[] array, Comparator solver)
+        {
+            if (array == null)
+                throw new ArgumentNullException("Object cannot be null!");
+            SortLogics(ref array, 0, array.Length, solver);
+        }
+        public static void Sort(ref SingleRow[] array, int startPoint, int endPoint, Comparator solver)
+        {
+            if (array == null)
+                throw new ArgumentNullException("Object cannot be null!");
+            SortLogics(ref array, startPoint, endPoint, solver);
+        }
+        private static void SortLogics(ref SingleRow[] array, int startPoint, int endPoint, Comparator solver)
+        {
+            for (int i = startPoint; i < endPoint; i++)
+            {
+                for (int j = 0; j < array.Length - 1 - i; j++)
+                {
+                    if (solver(array[j], array[j + 1]))
+                    {
+                        Swap(ref array[j], ref array[j + 1]);
+                    }
+                }
+            }
+        }
+        private static void Swap(ref SingleRow firstArg, ref SingleRow secondArg)
+        {
+            SingleRow tmpArg = firstArg;
+            firstArg = secondArg;
+            secondArg = tmpArg;
+        }
+    }
+    internal struct SingleRow
+    {
+        public int key;
+        public int[] row;
+    }
+}
---
+++ PolynomialMath/Polynomial.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolynomialMath
+{
+    public class Polynomial
+    {
+        private double[] coefficients;
+        public Polynomial(double[] initialParams)
+        {
+            if (initialParams != null && initialParams.Length != 0)
+            {
+                coefficients = new double[initialParams.Length];
+                initialParams.CopyTo(coefficients, 0);
+            }
+            else
+            {
+                coefficients = new double[] { 0 };
+            }            
+        }
+        public static Polynomial operator +(Polynomial firstObj, Polynomial secondObj)
+        {
+            int firstPolLength = firstObj.coefficients.Length;
+            int secondPolLength = secondObj.coefficients.Length;
+            int size = firstPolLength > secondPolLength ? firstPolLength : secondPolLength;
+            double[] newCoeffArray = new double[size];
+            for (int i = 0; i < size; i++)
+            {
+                if (firstPolLength != 0 && secondPolLength != 0)
+                {
+                    newCoeffArray[i] = firstObj.coefficients[i] + secondObj.coefficients[i];
+                    firstPolLength--;
+                    secondPolLength--;
+                }
+                else if (firstPolLength == 0)
+                {
+                    newCoeffArray[i] = secondObj.coefficients[i];
+                }
+                else
+                {
+                    newCoeffArray[i] = firstObj.coefficients[i];
+                }
+            }
+            return new Polynomial(newCoeffArray);
+        }
+        public static Polynomial operator -(Polynomial firstObj, Polynomial secondObj)
+        {
+            int firstPolLength = firstObj.coefficients.Length;
+            int secondPolLength = secondObj.coefficients.Length;
+            int size = firstPolLength > secondPolLength ? firstPolLength : secondPolLength;
+            double[] newCoeffArray = new double[size];
+            for (int i = 0; i < size; i++)
+            {
+                if (firstPolLength != 0 && secondPolLength != 0)
+                {
+                    newCoeffArray[i] = firstObj.coefficients[i] - secondObj.coefficients[i];
+                    firstPolLength--;
+                    secondPolLength--;
+                }
+                else if (firstPolLength == 0)
+                {
+                    newCoeffArray[i] = -secondObj.coefficients[i];
+                }
+                else
+                {
+                    newCoeffArray[i] = firstObj.coefficients[i];
+                }
+            }
+            return new Polynomial(newCoeffArray);
+        }
+        public static Polynomial operator *(Polynomial firstObj, Polynomial secondObj)
+        {
+            int firstPolLength = firstObj.coefficients.Length;
+            int secondPolLength = secondObj.coefficients.Length;
+            int size = firstPolLength + secondPolLength - 1;
+            double[] newCoeffArray = new double[size];
+            for (int i = 0; i < firstPolLength; i++)
+                for (int j = 0; j < secondPolLength; j++)
+                    newCoeffArray[i + j] += firstObj.coefficients[i] * secondObj.coefficients[j];
+            return new Polynomial(newCoeffArray);
+        }
+        public static bool operator ==(Polynomial firstObj, Polynomial secondObj)
+        {
+            int firstPolLength = firstObj.coefficients.Length;
+            int secondPolLength = secondObj.coefficients.Length;
+            if (firstPolLength != secondPolLength)
+                return false;
+            for (int i = 0; i < firstPolLength; i++)
+                if (firstObj.coefficients[i] != secondObj.coefficients[i])
+                    return false;
+            return true;
+        }
+        public static bool operator !=(Polynomial firstObj, Polynomial secondObj)
+        {
+            int firstPolLength = firstObj.coefficients.Length;
+            int secondPolLength = secondObj.coefficients.Length;
+            if (firstPolLength != secondPolLength)
+                return true;
+            for (int i = 0; i < firstPolLength; i++)
+                if (firstObj.coefficients[i] != secondObj.coefficients[i])
+                    return true;
+            return false;
+        }
+        public override string ToString()
+        {
+            
+            string polView = coefficients[0].ToString();
+            for(int i = 1; i <coefficients.Length; i++)
+                polView += " + " + coefficients[i] + "x^" + i;
+            return polView;
+        }
+        public override bool Equals(Object obj)
+        {
+            return (this == obj);
+        }
+        public override int GetHashCode()
+        {
+            int hashcode = 0;
+            for(int i = 0; i < coefficients.Length; i++)
+                hashcode = 31 * hashcode + coefficients[i].GetHashCode();
+            return hashcode;
+        }
+    }
+}
