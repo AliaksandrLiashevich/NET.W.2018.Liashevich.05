8f868617fed8940334974ee10e78febf75feac46
AliaksandrLiashevich <43993109+AliaksandrLiashevich@users.noreply.github.com>
2018-11-10 01:10:28 +0300

    Code correction

---
+++ JaggedArraySorts/JaggedSorts.Tests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace SortJaggedArrayLibrary.Tests
+{
+    [TestFixture]
+
+    public class JaggedSortsTests
+    {
+        int[][] jaggedArray = new int[][]
+        {
+            new int[] { 1, 3, 5, 7, 9 },
+            new int[] { 0, 2, 4, 6 },
+            new int[] { 11, 22 }
+        };
+        
+        [Test]
+
+        public void SumOfRowSort_SortByRowSum_SotrtedArray()
+        {
+            int[][] expected = new int[][]
+            {
+                new int[] { 11, 22 },
+                new int[] { 1, 3, 5, 7, 9 },
+                new int[] { 0, 2, 4, 6 },
+            };
+
+            Helper(expected, false);
+        }
+
+        [Test]
+
+        public void MaxRowValueSort_SortByMaxRowValue_SotrtedArray()
+        {
+            int[][] expected = new int[][]
+            {
+                new int[] { 0, 2, 4, 6 },
+                new int[] { 1, 3, 5, 7, 9 },
+                new int[] { 11, 22 }
+            };
+
+            Helper(expected, true);
+        }
+
+        [Test]
+
+        public void MinRowValueSort_SortByMinRowValue_SotrtedArray()
+        {
+            int[][] expected = new int[][]
+            {
+                new int[] { 11, 22 },
+                new int[] { 1, 3, 5, 7, 9 },
+                new int[] { 0, 2, 4, 6 },
+            };
+
+            Helper(expected, false);
+        }
+
+        private void Helper(int[][] expected, bool mode)
+        {
+            int[][] localJagged = new int[jaggedArray.GetLength(0)][];
+
+            Array.Copy(jaggedArray, localJagged, jaggedArray.Length);
+
+            JaggedSorts.SumOfRowSort(ref localJagged, mode);
+
+            Assert.AreEqual(expected, localJagged);
+        }
+    }
+}
--- JaggedArraySorts/JaggedSorts.cs
+++ JaggedArraySorts/JaggedSorts.cs
@@ -1,58 +1,100 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SortJaggedArrayLibrary
 {
-    delegate bool Comparator(SingleRow firstArg, SingleRow secondArg);
+    internal delegate bool Comparator(SingleRow firstArg, SingleRow secondArg);
+
     public static class JaggedSorts
     {
         static Comparator solver = null;
         static SingleRow[] keysArray = null;
+
+        /// <summary>
+        /// Sort jagged array by sum of the row
+        /// </summary>
+        /// <param name="jaggedArray">Sortable array</param>
+        /// <param name="mode">ASC | DESC mode</param>
         public static void SumOfRowSort(ref int[][] jaggedArray, bool mode)
         {
             keysArray = new SingleRow[jaggedArray.GetLength(0)];
+
             SumRowValue(jaggedArray);
+
             SetSortMode(mode);
+
             BubleSort.Sort(ref keysArray, solver);
+
             ArraySwapRows(ref jaggedArray);
         }
+
+        /// <summary>
+        /// Sort jagged array by max value of the row
+        /// </summary>
+        /// <param name="jaggedArray">Sortable array</param>
+        /// <param name="mode">ASC | DESC mode</param>
         public static void MaxRowValueSort(ref int[][] jaggedArray, bool mode)
         {
             keysArray = new SingleRow[jaggedArray.GetLength(0)];
+
             MaxRowValue(jaggedArray);
+
             SetSortMode(mode);
+
             BubleSort.Sort(ref keysArray, solver);
+
             ArraySwapRows(ref jaggedArray);
         }
+
+
+        /// <summary>
+        /// Sort jagged array by max value of the row
+        /// </summary>
+        /// <param name="jaggedArray">Sortable array</param>
+        /// <param name="mode">ASC | DESC mode</param>
         public static void MinRowValueSort(ref int[][] jaggedArray, bool mode)
         {
             keysArray = new SingleRow[jaggedArray.GetLength(0)];
+
             MinRowValue(jaggedArray);
+
             SetSortMode(mode);
+
             BubleSort.Sort(ref keysArray, solver);
+
             ArraySwapRows(ref jaggedArray);
         }
+
+        /// <summary>
+        /// Calculates Sum of each row and save it into keys array
+        /// </summary>
+        /// <param name="jaggedArray">Sirtable array</param>
         private static void SumRowValue(int[][] jaggedArray)
         {
             for(int i = 0; i < jaggedArray.GetLength(0); i++)
             {
                 keysArray[i].row = jaggedArray[i];
+
                 for (int j = 0; j < jaggedArray[i].Length; j++)
                 {
                     keysArray[i].key += jaggedArray[i][j];
                 }
             }
         }
+
+        /// <summary>
+        /// Calculates max element of row and save it into keys array
+        /// </summary>
+        /// <param name="jaggedArray">Sirtable array</param>
         private static void MaxRowValue(int[][] jaggedArray)
         {
             int maxValue = 0;
+
             for (int i = 0; i < jaggedArray.GetLength(0); i++)
             {
                 maxValue = jaggedArray[i][0];
+
                 keysArray[i].row = jaggedArray[i];
+
                 for (int j = 0; j < jaggedArray[i].Length; j++)
                 {
                     if (maxValue < jaggedArray[i][j])
@@ -60,16 +102,25 @@ namespace SortJaggedArrayLibrary
                         maxValue = jaggedArray[i][j]; 
                     }
                 }
+
                 keysArray[i].key = maxValue;
             }
         }
+
+        /// <summary>
+        /// Calculates min element of row and save it into keys array
+        /// </summary>
+        /// <param name="jaggedArray">Sirtable array</param>
         private static void MinRowValue(int[][] jaggedArray)
         {
             int minValue = 0;
+
             for (int i = 0; i < jaggedArray.GetLength(0); i++)
             {
                 minValue = jaggedArray[i][0];
+
                 keysArray[i].row = jaggedArray[i];
+
                 for (int j = 0; j < jaggedArray[i].Length; j++)
                 {
                     if (minValue > jaggedArray[i][j])
@@ -77,9 +128,15 @@ namespace SortJaggedArrayLibrary
                         minValue = jaggedArray[i][j];
                     }
                 }
+
                 keysArray[i].key = minValue;
             }
         }
+
+        /// <summary>
+        /// Custom swap method for put rows on the right place
+        /// </summary>
+        /// <param name="jaggedArray">Sorted array</param>
         private static void ArraySwapRows(ref int[][] jaggedArray)
         {
             for (int i = 0; i < jaggedArray.GetLength(0); i++)
@@ -87,42 +144,101 @@ namespace SortJaggedArrayLibrary
                 jaggedArray[i] = keysArray[i].row;
             }
         }
+
+        /// <summary>
+        /// Set ascending or descending sort mode
+        /// </summary>
+        /// <param name="mode">Sortable array</param>
         private static void SetSortMode(bool mode)
         {
             if (mode)
+            {
                 solver = AscendingSortRules;
+            }
             else
+            {
                 solver = DescendingSortRules;
+            }
         }
+
+        /// <summary>
+        /// Set ascending sort mode
+        /// </summary>
+        /// <param name="firstArg">First comparable object</param>
+        /// <param name="secondArg">Second comparable object</param>
+        /// <returns>True(firstArg > secondArg) or False(firstArg > secondArg)</returns>
         private static bool AscendingSortRules(SingleRow firstArg, SingleRow secondArg)
         {
             if (firstArg.key > secondArg.key)
+            {
                 return true;
+            }
             else
+            {
                 return false;
+            }
         }
+
+        /// <summary>
+        /// Set decscending sort mode
+        /// </summary>
+        /// <param name="firstArg">First comparable object</param>
+        /// <param name="secondArg">Second comparable object</param>
+        /// <returns>True(firstArg > secondArg) or False(firstArg > secondArg)</returns>
         private static bool DescendingSortRules(SingleRow firstArg, SingleRow secondArg)
         {
             if (firstArg.key > secondArg.key)
+            {
                 return false;
+            }
             else
+            {
                 return true;
+            }
         }     
     }
+
     internal static class BubleSort
     {
+        /// <summary>
+        /// Sort one-dimensional array by buble sort
+        /// </summary>
+        /// <param name="array">Sortable array</param>
+        /// <param name="solver">Sort criteria</param>
         public static void Sort(ref SingleRow[] array, Comparator solver)
         {
             if (array == null)
+            {
                 throw new ArgumentNullException("Object cannot be null!");
+            }
+
             SortLogics(ref array, 0, array.Length, solver);
         }
+
+        /// <summary>
+        /// Sort one-dimensional array by buble sort in definite range
+        /// </summary>
+        /// <param name="array">Sortable array</param>
+        /// <param name="startPoint">Point of beginning work sort algorithm</param>
+        /// <param name="endPoint">Point of ending work sort algorithm</param>
+        /// <param name="solver">Sort criteria</param>
         public static void Sort(ref SingleRow[] array, int startPoint, int endPoint, Comparator solver)
         {
             if (array == null)
+            {
                 throw new ArgumentNullException("Object cannot be null!");
+            }
+
             SortLogics(ref array, startPoint, endPoint, solver);
         }
+
+        /// <summary>
+        /// Main sort method of buble sort
+        /// </summary>
+        /// <param name="array">Sortable array</param>
+        /// <param name="startPoint">Point of beginning work sort algorithm</param>
+        /// <param name="endPoint">Point of ending work sort algorithm</param>
+        /// <param name="solver">Sort criteria</param>
         private static void SortLogics(ref SingleRow[] array, int startPoint, int endPoint, Comparator solver)
         {
             for (int i = startPoint; i < endPoint; i++)
@@ -136,13 +252,25 @@ namespace SortJaggedArrayLibrary
                 }
             }
         }
+
+        /// <summary>
+        /// Method that swap elements in sortable array
+        /// </summary>
+        /// <param name="firstArg">First argument</param>
+        /// <param name="secondArg">Second argument</param>
         private static void Swap(ref SingleRow firstArg, ref SingleRow secondArg)
         {
             SingleRow tmpArg = firstArg;
+
             firstArg = secondArg;
+
             secondArg = tmpArg;
         }
     }
+
+    /// <summary>
+    /// Struct for comfortable using one-dimensional buble sort
+    /// </summary>
     internal struct SingleRow
     {
         public int key;
